62c777af100035cba69693e310a0c995abf68def
michel kadi <michelkadi@mss-MacBook.local>
2024-05-21 17:18:39 +0300

    modified1

--- house.py
+++ house.py
@@ -1,11 +1,29 @@
 name = input("whats your name : ")
-if name == "Harry" or name == "ron" or name == "bor":
-    print("gryffinder")
-# elif name == "ron":
+# if name == "Harry" or name == "ron" or name == "bor":
 #     print("gryffinder")
-# elif name == "bor":
-#     print("gryffinder")
-elif name == "drace":
-    print("Elyffinder")
-else:
-    print("who ?")
+# # elif name == "ron":
+# #     print("gryffinder")
+# # elif name == "bor":
+# #     print("gryffinder")
+# elif name == "drace":
+#     print("Elyffinder")
+# else:
+#     print("who ?")
+#match keyword
+match name:
+    case "harry" | "hermione" | "ron":
+        print("gryffindor")
+        # | = or logical operator
+        
+    # case "hermione":
+    #     print("gryffindor")
+    # case "ron":
+    #     print("gryffindor")
+    case "draco":
+        print("stytherin")
+    case _:
+        print("who ?")
+    
+
+
+
