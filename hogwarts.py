9590836bc0805d95d37ffe0c90b070852561aa07
michel kadi <michelkadi@mss-MacBook.local>
2024-05-22 12:01:40 +0300

    new 4

--- hogwarts.py
+++ hogwarts.py
@@ -1,6 +1,20 @@
 #https://youtu.be/nLRL_NcnK-4?t=12083
 #-------list----
+# students = ["hermione","harry","ron"]
+# print(students[0])
+# print(students[1])
+# print(students[2])
+#------instead u can use loop---
+# for student in students:
+#     print(student)
+#--------list iteration-------
+#https://youtu.be/nLRL_NcnK-4?t=12340
 students = ["hermione","harry","ron"]
-print(students[0])
-print(students[1])
-print(students[2])
+for i in range(len(students)):
+    print(i + 1, students[i])
+
+for i in [0, 1, 2]:
+    print("Student")
+
+
+
