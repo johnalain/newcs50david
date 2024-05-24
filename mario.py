0d95c1d094b9db6f09b103f3f1520190c13e70d7
michel kadi <michelkadi@mss-MacBook.local>
2024-05-24 08:41:50 +0300

    hk1

---
+++ mario.py
@@ -0,0 +1,33 @@
+# def main ():
+#     print_column(3)
+
+# def print_column(height):
+#     # for _ in range(height):
+#     #     print("#") same output
+#     print("#\n" * height, end="") 
+
+# main()
+#https://youtu.be/nLRL_NcnK-4?t=14229
+# def main():
+#     print_row(4)
+# def print_row(width):
+#     print("?" * width)
+# main()
+#-----------build square-----
+def main():
+    print_square(5)
+def print_square(size):
+    #for each row in square
+    for i in range(size):
+        print_row(size)
+    # def print_row(width):
+    #    print("#" * width)
+    #for each brick in square
+# for j in range(size):
+#print brick
+# print("#", end = "")
+# print("#" * size)
+def print_row(width):
+     print("#" * width)
+main()
+#https://youtu.be/nLRL_NcnK-4?t=14762
