6d7a53142480e95e22f996703476c2318558a29c
michel kadi <michelkadi@mss-MacBook.local>
2024-05-27 16:08:55 +0300

    mod files

---
+++ Rita.java
@@ -0,0 +1,38 @@
+//https://youtu.be/_Hr_X7SwpWQ
+//----extracting substring--
+public class Rita{
+    public static void main(String[] args) {
+        String m1 = "welcome to java";
+        String m2 = m1.substring(0,11)+"html";
+        System.out.println(m1);
+        System.out.println(m2);
+        System.out.println(m1.substring(11)); 
+        String s1 = "welcome";
+        String s2 = "welcome" ; 
+        // if( s1.equals( s2)){ another way to compare 2 strings
+        if(s1 == s2){
+            System.out.println("are equal");
+        } else{
+            System.out.println("not equal");
+        } 
+        //https://youtu.be/_Hr_X7SwpWQ?t=1271
+        //------String conversion
+        //--the contents of the string can be changed ounce the STRING iscreated but you can convert a string to a new string using methods
+        // String m = "Michel Kadi";
+        // System.out.println(m.toUpperCase());
+        // System.out.println(m.toLowerCase());
+        // System.out.println(m.replace("Kadi", "josephine"));
+        // System.out.println(m.replace(m,"josephine rita"));
+        // output:
+//  MICHEL KADI
+// michel kadi
+// Michel josephine
+// josephine rita
+//finding a character or Substring in a string
+//https://youtu.be/_Hr_X7SwpWQ?t=1476
+
+                 
+        
+    }
+}
+
---
+++ hello.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("whats your name? :")
+    print(hello(name))
+    
+def hello(to="world"):
+    return f"hello, {to}"
+if __name__ == "__main__":
+    main()
+    #https://youtu.be/nLRL_NcnK-4?t=24835 6:49
+    
