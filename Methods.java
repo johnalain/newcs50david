d00caa88151e43c412933ec0aa9837a44f9381a4
michel kadi <michelkadi@mss-MacBook.local>
2024-05-28 10:15:11 +0300

    all

---
+++ Methods.java
@@ -0,0 +1,41 @@
+//https://youtu.be/cCgOESMQe44?list=PLlvFEn0NKwXI7Lc0Zb1WO89Pcz3LNYHhi&t=69
+public class Methods {
+    public static void main(String[] args) {
+        // int a = 5;
+        // int b = 6;
+        // System.out.println(a *b);
+        // int c = 4;
+        // int d = 2;
+        // System.out.println(c *d);
+        //instead create a method call it Multiply
+        Welcome();
+        divide(100,5);
+        divide(66,11);
+        divide(200,10);
+
+        multiply(5, 10);
+        multiply(2, 14);
+        multiply(6, 15);
+        mik();
+        // divide();
+    }
+    public static void Welcome() {
+        System.out.println("welcome to my calculator");
+        
+    }
+    public static void multiply( float a, float b) {
+        System.out.println(a * b);
+
+    }
+    public static void mik() {
+        System.out.println("hello, world!");
+        
+    }
+    public static void divide(int a,int b){
+        System.out.println(a / b);
+    } 
+        
+    
+    
+}
+// https://www.youtube.com/playlist?list=PL59LTecnGM1NRUyune3SxzZlYpZezK-oQ java complete course
